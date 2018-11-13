dcf48b1198107b9cbc990438c57d48538a980aa1
wjz <jianzhong.wang@baifendian.com>
2018-11-13 11:29:35 +0800

    还差一点特效动画

--- src/Andeputai/Header/Header.js
+++ src/Andeputai/Header/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Icon } from 'antd';
+import { detectmob } from '../../utils/utils.js'
 import './header.scss';
 
 const {Header: AHeader} = Layout;
@@ -7,6 +8,11 @@ const {Header: AHeader} = Layout;
 class Header extends Component {
   constructor(props) {
     super(props);
+
+
+    this.state = {
+      collapsed: true,
+    }
 
     this.navList = [
       {
@@ -32,35 +38,79 @@ class Header extends Component {
     ];
   }
 
-  createHeader() {
+  createPCHeader() {
     return(
-      <Menu
-        theme="dark"
-        mode="inline"
-        style={{ lineHeight: '64px' }}
-      >
-        {
-          this.navList.map(nav => (
-            <Menu.Item key={nav.name}>
-              <a href={nav.anchor}>
-                {nav.name}
-              </a>
-            </Menu.Item>
-          ))
-        }
-      </Menu>
+      <div>
+        <div className="logo" />
+
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          style={{ lineHeight: '64px' }}
+        >
+          {
+            this.navList.map(nav => (
+              <Menu.Item key={nav.name}>
+                <a href={nav.anchor}>
+                  {nav.name}
+                </a>
+              </Menu.Item>
+            ))
+          }
+        </Menu>
+      </div>
+    );
+  }
+
+  createMobHeader() {
+    const {collapsed} = this.state;
+
+    const toggleCollapsed = () => {
+      this.setState({
+        collapsed: !collapsed
+      });
+    };
+
+
+
+
+    return (
+      <div className="menu">
+        <div className="menu-title">
+          <div className="logo" />
+
+          <div className="menu-btn" onClick={toggleCollapsed}>
+            <Icon type={collapsed ? 'menu-unfold' : 'menu-fold'} />
+          </div>
+        </div>
+
+        <div className={'menu-content ' + (collapsed ? 'content-hide' : 'content-show') }
+          id="menu-selector"
+        >
+          <Menu
+            theme="dark"
+            mode="inline"
+            style={{ lineHeight: '64px' }}
+          >
+            {
+              this.navList.map(nav => (
+                <Menu.Item key={nav.name}>
+                  <a href={nav.anchor}>
+                    {nav.name}
+                  </a>
+                </Menu.Item>
+              ))
+            }
+          </Menu>
+        </div>
+
+      </div>
     );
   }
 
   render() {
     return (
-      <div>
-        <div className="logo" />
-
-        {this.createHeader()}
-
-      </div>
-      
+      detectmob() ? this.createMobHeader() : this.createPCHeader()
     );
   }
 }
